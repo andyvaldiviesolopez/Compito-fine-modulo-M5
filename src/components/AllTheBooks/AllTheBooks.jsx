9badb31e4c49a398df9614df97c28d17634053cd
Andy Valdivieso <andyvaldiviesolopez@gmail.com>
2026-05-19 20:27:21 +0200

    W2 D1

--- src/components/AllTheBooks/AllTheBooks.jsx
+++ src/components/AllTheBooks/AllTheBooks.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HorrorBooks from "../../books/horror.json"
 import SingleBook from "../SingleBook/SingleBook.jsx"
+import Alert from 'react-bootstrap/Alert';
 
 const AllTheBooks = () => {
-
+    const [search, setSearch] = useState("")
+    const filteredBooks = HorrorBooks.filter((book) =>
+        book.title.toLowerCase().includes(search.toLowerCase())
+    )
     return (
         <div>
+            <input
+                type="text"
+                placeholder="Cerca un libro"
+                className="input input-text mb-5"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            {filteredBooks.length === 0 && (
+                <Alert variant="warning">
+                    Nessun libro trovato
+                </Alert>
+            )}
+
             <div className="row">
-                {HorrorBooks.map((book) => (
+                {filteredBooks.map((book) => (
                     <div
                         key={book.asin}
-                        className="col-12 col-sm-6 col-md-4 col-lg-3 mb-5"
+                        className="col-12 col-sm-6 col-md-4 col-lg-3"
                     >
                         <SingleBook book={book} />
                     </div>
@@ -19,5 +36,9 @@ const AllTheBooks = () => {
         </div>
     )
 }
+
+/*<Alert variant="primary" className="mt-5">
+                <Alert.Heading>Hey, this is an Alert!</Alert.Heading>
+            </Alert> */
 
 export default AllTheBooks
